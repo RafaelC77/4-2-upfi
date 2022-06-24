7b491cbf823693b3a52ab0f7be7a6ef36232dfc6
Rafael Guimaraes <rcarvalhobsb@gmail.com>
2022-06-24 10:35:03 -0300

    challenge resolution

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, useDisclosure } from '@chakra-ui/react';
+import { Grid, useDisclosure } from '@chakra-ui/react';
 import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
@@ -18,15 +18,40 @@ interface CardsProps {
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   // TODO SELECTED IMAGE URL STATE
 
+  const [selectedImage, setSelectedImage] = useState('');
+
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    onOpen();
+    setSelectedImage(url);
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
-
+      <Grid templateColumns="repeat(3, 1fr)" gap={10}>
+        {cards.map(card => {
+          return (
+            <Card
+              key={card.id}
+              data={card}
+              viewImage={() => {
+                handleViewImage(card.url);
+              }}
+            />
+          );
+        })}
+      </Grid>
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImage}
+      />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -6,6 +6,7 @@ import {
   ModalBody,
   Image,
   Link,
+  Flex,
 } from '@chakra-ui/react';
 
 interface ModalViewImageProps {
@@ -20,4 +21,29 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent bgColor="pGray.800">
+        <ModalBody>
+          <Image
+            src={imgUrl}
+            maxW="900px"
+            maxH="600px"
+            w="100%"
+            h="100%"
+            objectFit="cover"
+          />
+        </ModalBody>
+
+        <ModalFooter>
+          <Flex justify="start" h="100%" w="100%">
+            <Link href={imgUrl} isExternal>
+              Abrir original
+            </Link>
+          </Flex>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
